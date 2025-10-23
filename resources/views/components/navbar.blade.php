4dfe5a79456e64a93e988bae22616d32487f5e54
John Rivs <hi@johnrivs.com>
2025-10-23 17:33:33 +0200

    Dropdown menu en el dashboard

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,14 +10,23 @@
                     <li class="nav-item"><a class="nav-link" href="{{ route('session.create') }}">Login</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                 @else
-                    <li class="nav-item"><a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a></li>
-                    <li class="nav-item">
-                        <form method="POST" action="{{ route('session.destroy') }}">
-                            @csrf
-                            @method('DELETE')
+                    <li class="dropdown" x-data="{ open: false }">
+                        <button class="btn btn-secondary text-nowrap" type="button" @click="open = ! open">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="dropdown-icon">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        </button>
+                        <ul class="dropdown-menu end-0 mt-1" :class="{ 'd-block': open }">
+                            <li><a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a></li>
+                            <li>
+                                <form method="POST" action="{{ route('session.destroy') }}">
+                                    @csrf
+                                    @method('DELETE')
 
-                            <button type="submit" class="nav-link btn btn-link">Logout</button>
-                        </form>
+                                    <button type="submit" class="dropdown-item">Logout</button>
+                                </form>
+                            </li>
+                        </ul>
                     </li>
                 @endguest
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\SessionController;
 use App\Livewire\Auth\Register;
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
@@ -16,7 +17,7 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::delete('/logout', [SessionController::class, 'destroy'])->name('session.destroy');
 
-    Route::get('/dashboard', function () {})->name('dashboard');
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
 
 
